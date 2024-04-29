1e9316faa8e8d354fa53435c0073eb1d7c70a83e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-28 21:06:57 -0600

    update(types): include new derivate types
     - NewUser
     - SafeUser
     - LoginData
     - AuthResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,25 @@
-export interface NewUser {
+export interface User {
+  id: number;
   Name: string;
   LastName: string;
   UserName: string;
   Email: string;
   Password: string;
   Role: number;
+}
+
+export interface NewUser extends Omit<User, "id"> { };
+
+export interface SafeUser extends Omit<User, "Password"> { };
+
+export interface LoginData extends Pick<User, "Email" | "Password"> { };
+
+export interface AuthResponse {
+  user: SafeUser | null;
+  error?: {
+    message: string;
+  },
+  message?: string;
 }
 
 export interface Category {
