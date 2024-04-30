079e4516a6d659d1b46e25a3842af4907dabdcf7
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-30 09:34:50 -0600

    update(components): application menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import { loggedUser } from '@/stores/UserStore'
+import { getUserAvatarURL } from '@/utils';
+import React, { useState } from 'react'
 
 export const Menu = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const $user = loggedUser.get();
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  }
+
+  const signout = () => {
+    loggedUser.set(null);
+
+    location.href = "/feed";
+  }
+
+  const menuIconClass = isMenuVisible ? 'fa-times' : 'fa-bars';
+  const dropdownClassNameState = isMenuVisible ? "opacity-100 translate-y-0 pointer-events-auto z-10" : "opacity-0 translate-y-[-10px] z-[-1]  pointer-events-none";
+
+
+
   return (
-    <div>Menu</div>
+    <section className="relative flex gap-4 items-center">
+
+      <section className="hidden md:flex items-center justify-center gap-2">
+        {
+          $user && <h3>{$user.Name} {$user.LastName}</h3>
+        }
+        {
+          $user && <img className="rounded-full" src={getUserAvatarURL($user.Name, $user.LastName)} width={32} height={32} />
+        }
+      </section>
+
+      <section className={`flex flex-col absolute top-20 right-0 bg-brand-blue-700 rounded-md p-2 w-52 gap-2 divide-y divide-red-800 ${dropdownClassNameState} transition-all duration-300 ease-in-out`}>
+
+        <section className="flex items-center justify-center gap-2 p-2 lg:hidden">
+          {
+            $user && <h3>{$user.Name} {$user.LastName}</h3>
+          }
+          {
+            $user && <img className="rounded-full" src={getUserAvatarURL($user.Name, $user.LastName)} width={32} height={32} />
+          }
+        </section>
+
+        <h3 className="text-center p-2">Opciones</h3>
+
+        <section>
+          <a className="block text-center bg-brand-yellow rounded-md mt-2 text-black p-2 w-full" href="/">Inicio</a>
+          <button onClick={signout} className="bg-brand-yellow rounded-md mt-2 text-black p-2 w-full">Cerrar sesión</button>
+        </section>
+      </section>
+
+      <span onClick={toggleMenu} className="inline-flex transition-all duration-300 ease-in-out">
+        <i className={`fas fa-xl ${menuIconClass}`}></i>
+      </span>
+    </section>
   )
 }
