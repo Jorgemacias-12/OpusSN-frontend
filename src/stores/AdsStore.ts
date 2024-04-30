6f229180fe9b55e5eb6a281634caa094f9f242ff
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-30 02:04:04 -0600

    update(stores): AdsStore
    Change url prefix to ads/

--- src/stores/AdsStore.ts
+++ src/stores/AdsStore.ts
@@ -5,25 +5,25 @@ export const $adCampaigns = atom<AdCampaign[]>([
   {
     id: "ads-01",
     name: "CluckinBell-GTA-SA",
-    url: "/AdCampaign-CluckinBell-GTASA.webp",
+    url: "ads/AdCampaign-CluckinBell-GTASA.webp",
     alt: "Campaña marketing GTA:SA"
   },
   {
     id: "ads-02",
     name: "dekoch-GTA-SA",
-    url: "/AdCampaign-DeKoch-GTASA.webp",
+    url: "ads/AdCampaign-DeKoch-GTASA.webp",
     alt: "Campaña marketing 2 GTA:SA"
   },
   {
     id: "ads-03",
     name: "Sprunk-GTA-V",
-    url: "/AdCampaign-Lifeinvader-GTAV.webp",
+    url: "ads/AdCampaign-Lifeinvader-GTAV.webp",
     alt: "Campaña marketing GTA:V"
   },
   {
     id: "ads-04",
     name: "RedWood-GTA-IV",
-    url: "/AdCampaign-RedwoodCigarettes-GTAIV.webp",
+    url: "ads/AdCampaign-RedwoodCigarettes-GTAIV.webp",
     alt: "Campaña marketing GTA:IV"
   }
 ]);
